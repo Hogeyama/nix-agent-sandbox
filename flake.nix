9658a23b93897134ce1e624f385cf683cffc0db8
hogeyama <gan13027830@gmail.com>
2026-04-03 02:02:05 +0900

    chore: Add nix-bundle-elf

--- flake.nix
+++ flake.nix
@@ -4,16 +4,27 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    nix-bundle-elf.url = "github:Hogeyama/nix-bundle-elf";
+    nix-bundle-elf.inputs.nixpkgs.follows = "nixpkgs";
+    nix-bundle-elf.inputs.flake-utils.follows = "flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs =
+    { nixpkgs
+    , flake-utils
+    , nix-bundle-elf
+    , ...
+    }:
     flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          packages = [ pkgs.deno ];
-        };
-      });
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.deno
+          nix-bundle-elf.packages.${system}.default
+        ];
+      };
+    });
 }
